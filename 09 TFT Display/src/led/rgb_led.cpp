a19d5ab75cca09d51f558ad70d270dcb4de4fbc8
Cysorn <maksym.fandyeyev@web.de>
2024-06-11 20:42:39 +0200

    Removed white space

--- 09 TFT Display/src/led/rgb_led.cpp
+++ 09 TFT Display/src/led/rgb_led.cpp
@@ -1,1 +1,15 @@
 #include "rgb_led.h"
+#include <Arduino.h>
+
+RgbLED::RgbLED()
+{
+    pinMode(LED_R, OUTPUT);              
+    pinMode(LED_G, OUTPUT);
+    pinMode(LED_B, OUTPUT);
+}
+
+void RgbLED::shineColor(short int red, short int green, short int blue){
+    digitalWrite(LED_R, red);
+    digitalWrite(LED_G, green);
+    digitalWrite(LED_B, blue);
+}
